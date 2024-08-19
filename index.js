bd040f79a2154986ce4c4eeb08a9337e55495b41
apola-islam <160167557+apola-islam@users.noreply.github.com>
2024-08-19 17:30:43 +0600

    changed from military time to normal

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function updateClock(){
     const timeNow=new Date();
     const hours=timeNow.getHours();
+    let meridien;
+    hours > 12 ? meridien = " PM": meridien = " AM"
     const minutes=timeNow.getMinutes();
     const seconds = timeNow.getSeconds();
-
-
-
-    const time = hours.toString().padStart(2,0) + ":" + minutes.toString().padStart(2,0) + ":" + seconds.toString().padStart(2,0);
-
+    const time = Math.abs(hours-12).toString().padStart(2,0) + ":" + minutes.toString().padStart(2,0) + ":" + seconds.toString().padStart(2,0) + meridien;
     document.getElementById("clock").textContent = time;
 }
 updateClock();
